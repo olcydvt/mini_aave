c26de52d981c104870a80ad9490becfd51954ee8
Olcay Davut Cabbas <olcay.d.cabbas@gmail.com>
2025-06-24 14:05:50 +0300

    improve LendingPool.test.js

--- test/LendingPool.test.js
+++ test/LendingPool.test.js
@@ -38,7 +38,6 @@ describe("LendingPool", function () {
     expect(await lendingPool.deposits(user1.address, await mockToken.getAddress())).to.equal(depositAmount);
   });
 
-
   it("Should allow borrowing within LTV", async function () {
     const depositAmount = ethers.parseEther("200");
     const borrowAmount = ethers.parseEther("100");
@@ -70,6 +69,9 @@ describe("LendingPool", function () {
     expect(snapshot.totalLiquidity).to.be.above(depositAmount);
     expect(snapshot.exchangeRate).to.be.above(ethers.parseEther("1"));
     expect(snapshot.accruedInterest).to.be.above(0);
+    expect(snapshot.totalBorrows).to.be.above(ethers.parseEther("500"));
+    expect(snapshot.borrowRate).to.be.above(0);
+    expect(snapshot.borrowIndex).to.be.above(0);
   });
 
   it("Should allow withdrawal", async function () {
@@ -78,7 +80,30 @@ describe("LendingPool", function () {
     await lendingPool.connect(user1).deposit(await mockToken.getAddress(), depositAmount);
 
     await lendingPool.connect(user1).withdraw(await mockToken.getAddress(), depositAmount);
-    expect(await lendingPool.deposits(user1.address, await mockToken.getAddress())).to.equal(0);
+    const deposit = await lendingPool.deposits(user1.address, await mockToken.getAddress())
+    console.log("aToken balance after withdraw:", deposit);
+    expect(Number(deposit)).to.eq(0);
     expect(await mockToken.balanceOf(user1.address)).to.equal(ethers.parseEther("1000"));
   });
+
+  it("Should allow repayment", async function () {
+    const depositAmount = ethers.parseEther("200");
+    const borrowAmount = ethers.parseEther("100");
+
+    await mockToken.connect(user1).approve(await lendingPool.getAddress(), depositAmount);
+    await lendingPool.connect(user1).deposit(await mockToken.getAddress(), depositAmount);
+
+    await mockToken.connect(user2).approve(await lendingPool.getAddress(), depositAmount);
+    await lendingPool.connect(user2).deposit(await mockToken.getAddress(), depositAmount);
+    await lendingPool.connect(user2).borrow(await mockToken.getAddress(), borrowAmount);
+
+    await mockToken.connect(user2).approve(await lendingPool.getAddress(), borrowAmount);
+    await lendingPool.connect(user2).repay(await mockToken.getAddress(), borrowAmount);
+
+    const userBorrow = await lendingPool.borrows(user2.address, await mockToken.getAddress());
+    expect(userBorrow.amount).to.equal(0);
+  });
 });
+
+// ### Usage
+//   yarn hardhat test
